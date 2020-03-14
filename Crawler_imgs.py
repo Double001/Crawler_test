d36b2fdcb44be76ac71a2f1434db18cd99cb0967
Double Huang <hft1810@qq.com>
2020-03-14 10:38:04 +0800

    delete file path

--- Crawler_imgs.py
+++ Crawler_imgs.py
@@ -16,7 +16,7 @@ for ul in img_url:
         html = img['src']
         r = requests.get(html, stream = True)
         img_name = html.split('/')[-1]
-        with open('/Users/huangfutian/Desktop/imgs/%s' % img_name, 'wb') as f:
+        with open('/Users/****/Desktop/imgs/%s' % img_name, 'wb') as f:
             for chunk in r.iter_content(chunk_size = 128):
                 f.write(chunk)
         print ('Saved %s' % img_name)
